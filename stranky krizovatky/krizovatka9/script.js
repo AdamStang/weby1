60255bf278cead99afc1df41de6189c80b6eb5ce
stetakova <58446616+stetakova@users.noreply.github.com>
2020-01-05 21:41:43 +0100

    Add files via upload

---
+++ stranky krizovatky/krizovatka9/script.js
@@ -0,0 +1,42 @@
+
+function animauto1(x) {
+    x.classList.add("pohybauto1");
+	x.classList.remove("stopauto1");
+	
+}
+
+function animauto2(x) {
+    x.classList.add("pohybauto2");
+	x.classList.remove("stopauto2");
+	
+}
+
+function animauto3(x) {
+    x.classList.add("pohybauto3");
+	x.classList.remove("stopauto3");
+	
+}
+
+function animauto4(x) {
+    x.classList.add("pohybauto4");
+	x.classList.remove("stopauto4");
+	
+}
+
+function animauto5(x) {
+    x.classList.add("pohybauto5");
+	x.classList.remove("stopauto5");
+	
+}
+
+function animelektricka1(x) {
+    x.classList.add("pohybelektricka1");
+	x.classList.remove("stopelektricka1");
+	
+}
+function animelektricka2(x) {
+    x.classList.add("pohybelektricka2");
+	x.classList.remove("stopelektricka2");
+	
+}
+
