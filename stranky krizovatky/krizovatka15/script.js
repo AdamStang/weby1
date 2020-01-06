9cdad3944fb4cd14b5deaf88914d9ef8e7d0bc30
stetakova <58446616+stetakova@users.noreply.github.com>
2020-01-06 01:48:18 +0100

    Add files via upload

---
+++ stranky krizovatky/krizovatka15/script.js
@@ -0,0 +1,19 @@
+
+function animauto() {
+	var auto1 = document.getElementById("auto1").classList.add("pohybauto1");
+	var auto2 = document.getElementById("auto2").classList.add("pohybauto2");	
+}
+
+function animpajac() {
+	var pajac11 = document.getElementById("pajac11").classList.add("apajac1");
+	var pajac21 = document.getElementById("pajac21").classList.add("apajac2");
+	var pajac11 = document.getElementById("pajac12").classList.add("apajac1");
+	var pajac21 = document.getElementById("pajac22").classList.add("apajac2");
+	var stopsemafor11 = document.getElementById("stopsemafor11").classList.add("asemafor1");
+	var stopsemafor21 = document.getElementById("stopsemafor21").classList.add("asemafor2");
+	var stopsemafor12 = document.getElementById("stopsemafor12").classList.add("asemafor1");
+	var stopsemafor22 = document.getElementById("stopsemafor22").classList.add("asemafor2");
+	
+}
+
+
